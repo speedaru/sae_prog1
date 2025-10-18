7c22d6ffc946be0543c2c1cef147530a8f558b3d
speedaru <esn4fre@gmail.com>
2025-10-18 04:05:29 +0200

    added dungeon rendering, modified asset_manager, and implemented block rotations

---
+++ src/config.py
@@ -0,0 +1,4 @@
+import os
+from pathlib import Path
+
+DUNGEON_FILES_DIR = os.path.join(Path(__file__).parent, "..", "dungeons")
--- src/game/main_game.py
+++ src/game/main_game.py
@@ -1,29 +1,42 @@
 import time
 
+from src.config import *
 import libs.fltk as fltk
-
 from src.utils.logging import log_release, log_debug, log_debug_full
 
 from src.engine.asset_manager import *
 import src.engine.engine_config as engine_config
 import src.engine.fps_manager as fps_manager
-import src.engine.asset_manager as asset_manager
+# import src.engine.asset_manager as asset_manager
+from src.engine.structs.dungeon import *
 
 import src.game.game_config as game_config
 import src.game.gui as gui
 
 
-def render():
+def render(dungeon: DungeonT | NoneType):
     if not asset_manager_initialized():
         asset_manager_init()
 
-    fltk.image_memoire(50.0, 50.0, asset_manager_get_block(BLOCK_DOUBLE_ADJACENT))
+    # only render if not NoneType and at least 1 row
+    if not isinstance(dungeon, NoneType) and len(dungeon) > 0:
+        dungeon_render(dungeon)
+
 
 def main_loop():
     last_frame_start = 0
 
     fps_interval_s: float = fps_manager.get_target_fps_interval_ms(engine_config.TARGET_FPS) / 1000
     log_debug_full(f"target fps interval ms: {fps_interval_s}")
+
+    dungeon: DungeonT = list()
+
+    # dungeon_file_name = "dungeon_easy.txt"
+    dungeon_file_name = "dungeon_hard.txt"
+    if not dungeon_parse_file(os.path.join(DUNGEON_FILES_DIR, dungeon_file_name), dungeon):
+        log_release(f"[main_loop] failed to parse {dungeon_file_name}")
+
+    dungeon_print_values(dungeon)
 
     gui.create_window()
 
@@ -35,8 +48,8 @@ def main_loop():
             break
 
         gui.start_render()
-        render()
+        render(dungeon)
         gui.render()
 
         time.sleep(fps_interval_s)
-        log_debug_full(f"fps: {fps_manager.get_fps_count(last_frame_start)}")
+        log_debug(f"fps: {fps_manager.get_fps_count(last_frame_start)}")
