9dfbfe2efbf342f631e87072dd3ddb2243551b09
speedaru <esn4fre@gmail.com>
2025-11-14 18:09:46 +0100

    added levels and made dragon and adventurer use same functions

--- src/engine/structs/dragon.py
+++ src/engine/structs/dragon.py
@@ -3,6 +3,7 @@ from random import randrange
 # engine stuff
 from src.engine.structs.dungeon import *
 from src.engine.asset_manager import *
+from src.engine.structs.entity import *
 
 # utils
 import src.utils.fltk_extensions as fltk_ext
@@ -19,25 +20,10 @@ DRAGON_COUNT = 2
 
 
 def dragon_init(dragon: DragonT, level: int = 1, room_pos: list[int] = [0, 0]):
-    if len(dragon) != DRAGON_COUNT:
-        dragon[:] = [None] * DRAGON_COUNT
-
-    dragon[DRAGON_LEVEL] = 1
-    dragon[DRAGON_ROOM_POS] = [randrange(0, 6), randrange(0, 6)]
+    room_pos = [randrange(0, 6), randrange(0, 6)]
+    entity_init(dragon, level, room_pos)
 
 def dragon_render(dragon: DragonT, assets: AssetsT):
-    knight_image = assets[ASSETS_CHARACTERS][CHARACTERS_DRAGONS]
+    dragon_image = assets[ASSETS_CHARACTERS][CHARACTERS_DRAGONS]
 
-    room_size = BLOCK_SCALED_SIZE
-    dragon_pos: list[int] = dragon[DRAGON_ROOM_POS]
-
-    # get draw position based on room location
-    draw_x = room_size[0] * dragon_pos[0] 
-    draw_y = room_size[1] * dragon_pos[1]
-
-    # add to draw position so we draw the dragon in the center of the room and not corner
-    draw_x += (room_size[0] / 2) - (CHARACTERS_SIZES[0] / 2)
-    draw_y += (room_size[1] / 2) - (CHARACTERS_SIZES[1] / 2)
-
-    log_debug_full(f"[dragon] (draw_x, draw_y): {(draw_x, draw_y)}")
-    fltk_ext.image_memoire(draw_x, draw_y, knight_image, ancrage="nw")
+    entity_render(dragon, dragon_image)
---
+++ src/engine/structs/entity.py
@@ -0,0 +1,50 @@
+# engine stuff
+from src.engine.structs.dungeon import *
+from src.engine.asset_manager import *
+
+# utils
+import src.utils.fltk_extensions as fltk_ext
+from src.utils.logging import *
+
+# types
+EntityT = list[list[int] | int]
+
+# enum for adventurer structure
+ENTITY_ROOM_POS = 0
+ENTITY_LEVEL = 1
+ENTITY_COUNT = 2
+
+
+def entity_init(entity: EntityT, level: int = 1, room_pos: list[int] = [0, 0]):
+    if len(entity) != ENTITY_COUNT:
+        entity[:] = [None] * ENTITY_COUNT
+
+    entity[ENTITY_LEVEL] = level
+    entity[ENTITY_ROOM_POS] = room_pos
+
+def entity_render(entity: EntityT, image):
+    room_size = BLOCK_SCALED_SIZE
+    entity_pos: list[int] = entity[ENTITY_ROOM_POS]
+
+    # get draw position based on room location
+    draw_x = room_size[0] * entity_pos[0] 
+    draw_y = room_size[1] * entity_pos[1]
+
+    # add to draw position so we draw the entity in the center of the room and not corner
+    draw_x += (room_size[0] / 2) - (CHARACTERS_SIZES[0] / 2)
+    draw_y += (room_size[1] / 2) - (CHARACTERS_SIZES[1] / 2)
+
+    # draw player
+    log_debug_full(f"[entity] (draw_x, draw_y): {(draw_x, draw_y)}")
+    fltk_ext.image_memoire(draw_x, draw_y, image, ancrage="nw")
+
+    # draw player level
+    TEXT_OFFSET = (CHARACTERS_SIZES[0] + 4, CHARACTERS_SIZES[1])
+    TEXT_POLICE_SIZE = 12
+    TEXT_SIZE = fltk.taille_texte(str(entity[ENTITY_LEVEL]), taille=TEXT_POLICE_SIZE)
+    TEXT_BG_SIZE = (TEXT_SIZE[0], TEXT_SIZE[1])
+
+    # render player level
+    text_pos_x, text_pos_y = draw_x + TEXT_OFFSET[0], draw_y
+    fltk.rectangle(text_pos_x, text_pos_y, text_pos_x + TEXT_BG_SIZE[0], text_pos_y + TEXT_BG_SIZE[1], "white", "white")
+    fltk.texte(text_pos_x, text_pos_y, str(entity[ENTITY_LEVEL]), ancrage="nw", taille=TEXT_POLICE_SIZE)
