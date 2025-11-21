2211431b073ded916baf91001e6b94b46a1a280c
speedaru <165056498+speedaru@users.noreply.github.com>
2025-11-21 16:49:04 +0100

    Update dragon.py

--- src/engine/structs/dragon.py
+++ src/engine/structs/dragon.py
@@ -80,16 +80,16 @@ def dragon_create_dragons(dragons: list[DragonT], dungeon_size: tuple[int, int],
     DRAGON_TO_ROOM_RATIO = 1 / 12
 
     if count == None:
-            area = dungeon_size[0] * dungeon_size[1]
-            count = round(DRAGON_TO_ROOM_RATIO * area)
-            count = max(count, 1) # at least 1 dragon
+        area = dungeon_size[0] * dungeon_size[1]
+        count = round(DRAGON_TO_ROOM_RATIO * area)
+        count = max(count, 1) # at least 1 dragon
 
     assert(not isinstance(count, NoneType)) # count not none
     assert(count > 0) # at least 1 dragon
 
     for _ in range(count):
-            d = DragonT()
-            random_room = [randrange(0, dungeon_size[0]), randrange(0, dungeon_size[1])]
-            log_debug_full(f"random room ({dungeon_size[0], dungeon_size[1]}): {random_room}")
-            dragon_init(d, room_pos=random_room)
-            dragons.append(d)
+        d = DragonT()
+        random_room = [randrange(0, dungeon_size[0]), randrange(0, dungeon_size[1])]
+        log_debug_full(f"random room ({dungeon_size[0], dungeon_size[1]}): {random_room}")
+        dragon_init(d, room_pos=random_room)
+        dragons.append(d)
